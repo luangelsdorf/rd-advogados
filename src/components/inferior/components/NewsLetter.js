8cfdca2a026f16d1331826db088d53b1ff2fbab5
luangelsdorf <luanferreira2136@gmail.com>
2022-04-07 20:28:20 -0300

    Sendgrid newsletter

--- src/components/inferior/components/NewsLetter.js
+++ src/components/inferior/components/NewsLetter.js
@@ -14,7 +14,6 @@ export default function NewsLetter({ textos }) {
     }
     let subscribed = document.getElementById('subscribed')
     let subscribing = document.getElementById('subscribing')
-    console.log('Sending')
     subscribing.style.display = 'flex'
 
     let data = {
@@ -50,7 +49,7 @@ export default function NewsLetter({ textos }) {
         <form onSubmit={handleSubmit}>
           <div className="input-group">
             <input onChange={(e) => { setEmail(e.target.value) }} required="required" type="email" className="form-control news-input" placeholder={'Insira seu E-mail'} />
-            <button className="btn btn-primary news-submit justify-content-center" type="submit">
+            <button className="btn btn-primary news-submit justify-content-center" type="submit" disabled={submitted}>
               <svg height="12" width="12" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right" className="svg-inline--fa fa-arrow-right fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path></svg>
             </button>
           </div>
--- src/pages/api/newsletter.js
+++ src/pages/api/newsletter.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+/* require('dotenv').config()
 
 export default function (req, res) {
   if (req.method === 'POST') {
@@ -40,4 +40,27 @@ export default function (req, res) {
   } else {
     res.status(405).send('405 - Method not Allowed.')
   }
+} */
+export default async (req, res) => {
+  if (req.method === 'POST') {
+
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const message = {
+      from: process.env.SENDER_EMAIL,
+      to: 'luanferreira2136@gmail.com',
+      subject: 'Nova Inscrição | Website R&D Advogados',
+      text: 'Esta foi enviada do seu website.',
+      html: `
+                <div>Nova inscrição recebida para a Newsletter:</div>
+                <p><strong>E-mail</strong>: ${req.body.email}</p>
+              `
+    }
+
+    await sgMail.send(message);
+    return res.status(200).end();
+  }
+
+  return res.status(404).end();
 }
