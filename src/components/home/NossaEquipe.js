962817a5e3cf3cdc588c51adfa6502b0ef572791
luangelsdorf <luanferreira2136@gmail.com>
2022-05-07 16:38:07 -0300

    Changed Member Photos appearance

--- src/components/home/NossaEquipe.js
+++ src/components/home/NossaEquipe.js
@@ -3,6 +3,10 @@ import Img from '../Img';
 
 export default function NossaEquipe({ content }) {
 
+  function handleClick(e) {
+    e.target.parentElement.parentElement.classList.toggle('expanded');
+  }
+
   return (
     <div className="container-fluid px-0 position-relative" id="nossa-equipe">
       <div className="container">
@@ -13,13 +17,30 @@ export default function NossaEquipe({ content }) {
         <div className="row gy-5">
           {
             content.members.map((member, index) => {
+              let splitted = member.bio.split('\n');
+              let firstPart = splitted.shift();
               return (
                 <div className={`col-12 col-xl-5 ${index === 0 ? 'offset-xl-1' : ''}`} key={`photo-${index}`}>
                   <div className="member-photo">
                     <Img src={member.photo.data.attributes} alt="" />
                     <div className="member-text">
                       <strong className="fs-24">{member.name}</strong>
-                      <p className="fs-14">{member.bio}</p>
+                      <p className="fs-14">
+                        {
+                          firstPart
+                        }
+                      </p>
+                      <p id={`collapse-${member.id}`} className="fs-14 collapse">
+                        {
+                          splitted.join('\n')
+                        }
+                      </p>
+                      <div
+                        data-bs-toggle="collapse"
+                        data-bs-target={`#collapse-${member.id}`}
+                        role="button"
+                        onClick={handleClick}
+                      />
                     </div>
                   </div>
                 </div>
